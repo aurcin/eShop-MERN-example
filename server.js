a8ccaacfd55bdad198fcc6c56ed82e3004d4e3d6
AurimasCi <aurcin@gmail.com>
2020-06-16 19:57:43 +0300

    added unhandled rejection handling and mongodb connection

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 require('dotenv').config();
 
+const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
+
 const PORT = process.env.PORT || 5000;
 const ENV = process.env.NODE_ENV;
 
+connectDB();
 const app = express();
 
 if (ENV === 'development') {
@@ -15,6 +19,14 @@ app.get('/', (req, res) => {
 	res.status(200).send('Hi from node');
 });
 
-app.listen(PORT, () => {
-	console.log(`Server started in ${ENV} mode on ${PORT} port`);
+app.use(errorHandler);
+
+const server = app.listen(
+	PORT,
+	console.log(`Server started in ${ENV} mode on ${PORT} port`),
+);
+
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`);
+	server.close(() => process.exit(1));
 });
