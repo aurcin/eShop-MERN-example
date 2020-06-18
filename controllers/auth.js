b1b3a2f7682c9f7fae9d76c1dd769ad25590ed64
AurimasCi <aurcin@gmail.com>
2020-06-19 00:47:36 +0300

    user login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const asyncHandler = require('../middleware/async');
+const ErrorResponse = require('../utils/errorResponse');
 
 // @desc    Register user
 // @route   POST /api/v1/auth/register
@@ -15,6 +16,34 @@ exports.register = asyncHandler(async (req, res, next) => {
 	});
 
 	sendTokenResponse(user, 201, res);
+});
+
+// @desc    Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+	const { email, password } = req.body;
+
+	// validate email & password
+	if (!email || !password) {
+		return next(new ErrorResponse('Please provide an email and password', 400));
+	}
+
+	// check for user (in model we set password select to false, but here we need it so we selecting it manualy)
+	const user = await User.findOne({ email }).select('+password');
+
+	if (!user) {
+		return next(new ErrorResponse('Invalid credentials', 401));
+	}
+
+	// check if password matches
+	const isMatch = await user.matchPassword(password);
+
+	if (!isMatch) {
+		return next(new ErrorResponse('Invalid credentials', 401));
+	}
+
+	sendTokenResponse(user, 200, res);
 });
 
 // Get token from model, create cookie and send response
