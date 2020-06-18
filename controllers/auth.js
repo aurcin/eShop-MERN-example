3f1350ed2dd20c8ad0af69a57533d037de7a0322
AurimasCi <aurcin@gmail.com>
2020-06-19 01:56:31 +0300

    user logout route

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,6 +58,21 @@ exports.getUser = asyncHandler(async (req, res, next) => {
 	});
 });
 
+// @desc    Logout current user, clear cookie
+// @route   GET /api/v1/auth/logout
+// @access  Private
+exports.logout = asyncHandler(async (req, res, next) => {
+	res.cookie('token', 'none', {
+		expires: new Date(Date.now() + 10 * 1000),
+		httpOnly: true,
+	});
+
+	res.status(200).json({
+		success: true,
+		data: {},
+	});
+});
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
 	// Create token
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
-const { register, login, getUser } = require('../controllers/auth');
+const { register, login, getUser, logout } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
+router.route('/').get(protect, getUser);
 router.route('/register').post(register);
 router.route('/login').post(login);
-router.route('/').get(protect, getUser);
+router.route('/logout').get(protect, logout);
 
 module.exports = router;
