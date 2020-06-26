edbfae541c493595c928521592df0f0754dfda25
AurimasCi <aurcin@gmail.com>
2020-06-26 13:10:35 +0300

    update product

--- controllers/products.js
+++ controllers/products.js
@@ -8,7 +8,7 @@ const ErrorResponse = require('../utils/errorResponse');
 // @desc    Create product
 // @route   POST /api/v1/products
 // @access  Private/Admin
-exports.create = async (req, res) => {
+exports.createProduct = async (req, res) => {
 	const form = new formidable.IncomingForm();
 	form.keepExtensions = true;
 	form.parse(req, async (err, fields, files) => {
@@ -40,6 +40,7 @@ exports.create = async (req, res) => {
 				data: product,
 			});
 		} catch (error) {
+			console.error(error);
 			return res.status(400).json({
 				success: false,
 				error: 'Invalid data entered',
@@ -67,6 +68,63 @@ exports.getProduct = asyncHandler(async (req, res, next) => {
 		data: product,
 	});
 });
+
+// @desc    Update product
+// @route   PUT /api/v1/products/:id
+// @access  Private/Admin
+exports.updateProduct = async (req, res) => {
+	const form = new formidable.IncomingForm();
+	form.keepExtensions = true;
+	form.parse(req, async (err, fields, files) => {
+		if (err) {
+			return res.status(400).json({
+				success: false,
+				error: 'Photo cannot be uploaded',
+			});
+		}
+
+		let product = await Product.findById(req.params.id);
+
+		if (!product) {
+			return res.status(400).json({
+				success: false,
+				error: 'Product not found',
+			});
+		}
+
+		if (files.photo) {
+			// size validation less than 1mb
+			if (files.photo.size > 1000000) {
+				return res.status(400).json({
+					success: false,
+					error: 'Photo size cannot extend 1MB',
+				});
+			}
+
+			product.photo.data = fs.readFileSync(files.photo.path);
+			product.photo.contentType = files.photo.type;
+		}
+
+		try {
+			product = await Product.findByIdAndUpdate(req.params.id, fields, {
+				new: true,
+				runValidators: true,
+			});
+
+			res.status(200).json({
+				success: true,
+				data: product,
+			});
+		} catch (error) {
+			console.error(error);
+			return res.status(400).json({
+				success: false,
+				error: 'Invalid data entered',
+				err: error,
+			});
+		}
+	});
+};
 
 // @desc    Delete product
 // @route   DELETE /api/v1/products/:id
--- routes/products.js
+++ routes/products.js
@@ -1,18 +1,20 @@
 const express = require('express');
 
 const {
-	create,
+	createProduct,
 	getProduct,
+	updateProduct,
 	deleteProduct,
 } = require('../controllers/products');
 const { protect, authorize } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.route('/').post(protect, authorize('admin'), create);
+router.route('/').post(protect, authorize('admin'), createProduct);
 router
 	.route('/:id')
 	.get(getProduct)
+	.put(updateProduct)
 	.delete(protect, authorize('admin'), deleteProduct);
 
 module.exports = router;
