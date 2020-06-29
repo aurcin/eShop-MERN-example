5fe2afa83f950a8b298e1aff0d1b0ff75028d00b
AurimasCi <aurcin@gmail.com>
2020-06-29 16:13:08 +0300

    error handling in advanced results

--- middleware/advancedResults.js
+++ middleware/advancedResults.js
@@ -1,73 +1,79 @@
+const ErrorResponse = require('../utils/errorResponse');
+
 const advancedResults = (model, populate) => async (req, res, next) => {
-	let query;
-	const reqQuery = { ...req.query };
+	try {
+		let query;
+		const reqQuery = { ...req.query };
 
-	// remove unvanted fields from query
-	const removeFields = ['select', 'sort', 'page', 'limit'];
-	removeFields.forEach((param) => delete reqQuery[param]);
+		// remove unvanted fields from query
+		const removeFields = ['select', 'sort', 'page', 'limit'];
+		removeFields.forEach((param) => delete reqQuery[param]);
 
-	let queryStr = JSON.stringify(reqQuery);
-	// adding $ to: lt, lte, gt, gte, in.
-	queryStr = queryStr.replace(
-		/\b(gt|gte|lt|lte|in)\b/g,
-		(match) => `$${match}`,
-	);
+		let queryStr = JSON.stringify(reqQuery);
+		// adding $ to: lt, lte, gt, gte, in.
+		queryStr = queryStr.replace(
+			/\b(gt|gte|lt|lte|in)\b/g,
+			(match) => `$${match}`,
+		);
 
-	query = model.find(JSON.parse(queryStr));
-	// if we pass select=param1,param2 and so on
-	if (req.query.select) {
-		const selectedfields = req.query.select.split(',').join(' ');
-		query = query.select(selectedfields);
-	}
+		query = model.find(JSON.parse(queryStr));
+		// if we pass select=param1,param2 and so on
+		if (req.query.select) {
+			const selectedfields = req.query.select.split(',').join(' ');
+			query = query.select(selectedfields);
+		}
 
-	// if we pass sort=field1,filed2,-field3 (-before param means desc sort) if no sort we seart by createdAt by default
-	if (req.query.sort) {
-		const sortBy = req.query.sort.split(',').join(' ');
-		query = query.sort(sortBy);
-	} else {
-		query = query.sort('-createdAt');
-	}
+		// if we pass sort=field1,filed2,-field3 (-before param means desc sort) if no sort we seart by createdAt by default
+		if (req.query.sort) {
+			const sortBy = req.query.sort.split(',').join(' ');
+			query = query.sort(sortBy);
+		} else {
+			query = query.sort('-createdAt');
+		}
 
-	// pagination
-	const page = parseInt(req.query.page, 10) || 1;
-	const limit = parseInt(req.query.limit, 10) || 100;
-	const startIndex = (page - 1) * limit;
-	const endIndex = page * limit;
-	const total = await model.countDocuments();
+		// pagination
+		const page = parseInt(req.query.page, 10) || 1;
+		const limit = parseInt(req.query.limit, 10) || 100;
+		const startIndex = (page - 1) * limit;
+		const endIndex = page * limit;
+		const total = await model.countDocuments();
 
-	query.skip(startIndex).limit(limit);
+		query.skip(startIndex).limit(limit);
 
-	if (populate) {
-		query = query.populate(populate);
-	}
+		if (populate) {
+			query = query.populate(populate);
+		}
 
-	// executing query
-	const results = await query;
+		// executing query
+		const results = await query;
 
-	// pagination next previous
-	const pagination = {};
-	if (endIndex < total) {
-		pagination.next = {
-			page: page + 1,
-			limit,
+		// pagination next previous
+		const pagination = {};
+		if (endIndex < total) {
+			pagination.next = {
+				page: page + 1,
+				limit,
+			};
+		}
+
+		if (startIndex > 0) {
+			pagination.prev = {
+				page: page - 1,
+				limit,
+			};
+		}
+
+		res.advancedResults = {
+			success: true,
+			count: results.length,
+			pagination,
+			data: results,
 		};
+
+		next();
+	} catch (error) {
+		return next(new ErrorResponse('Cannot get this data', 404));
 	}
-
-	if (startIndex > 0) {
-		pagination.prev = {
-			page: page - 1,
-			limit,
-		};
-	}
-
-	res.advancedResults = {
-		success: true,
-		count: results.length,
-		pagination,
-		data: results,
-	};
-
-	next();
 };
 
 module.exports = advancedResults;
