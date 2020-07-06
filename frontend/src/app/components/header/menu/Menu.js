6629ee551b84df6fb73950de2cb49495bcbc6020
AurimasCi <aurcin@gmail.com>
2020-07-07 02:16:40 +0300

    added react bootstrap styling

--- frontend/src/app/components/header/menu/Menu.js
+++ frontend/src/app/components/header/menu/Menu.js
@@ -1,32 +1,39 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const isActive = (history, path) => {
-	if (history.location.pathname === path) {
-		return { color: '#ff9900' };
-	}
-	return { color: '#000000' };
+	return history.location.pathname === path;
 };
 
 const Menu = ({ history }) => {
 	return (
-		<ul>
-			<li>
-				<Link to='/' style={isActive(history, '/')}>
-					Home
-				</Link>
-			</li>
-			<li>
-				<Link to='/signin' style={isActive(history, '/signin')}>
-					Sign In
-				</Link>
-			</li>
-			<li>
-				<Link to='/signup' style={isActive(history, '/signup')}>
-					Sign Up
-				</Link>
-			</li>
-		</ul>
+		<Navbar expand='lg' bg='dark' variant='dark' collapseOnSelect>
+			<LinkContainer to='/'>
+				<Navbar.Brand>E-SHOP</Navbar.Brand>
+			</LinkContainer>
+
+			<Navbar.Toggle aria-controls='main-navigation' />
+
+			<Navbar.Collapse id='main-navigation' className='justify-content-end'>
+				<Nav as='ul'>
+					<Nav.Item as='li'>
+						<LinkContainer to='/signin'>
+							<Nav.Link active={isActive(history, '/signin')}>Sign In</Nav.Link>
+						</LinkContainer>
+					</Nav.Item>
+
+					<Nav.Item as='li'>
+						<LinkContainer to='/signup'>
+							<Nav.Link active={isActive(history, '/signup')}>Sign Up</Nav.Link>
+						</LinkContainer>
+					</Nav.Item>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
 	);
 };
 
