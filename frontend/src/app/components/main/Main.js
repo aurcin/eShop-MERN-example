cb6c4866c48534fa19f4d1b2f65337e21e396e05
AurimasCi <aurcin@gmail.com>
2020-07-09 08:00:53 +0300

    added sign up form

--- frontend/src/app/components/main/Main.js
+++ frontend/src/app/components/main/Main.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Container from 'react-bootstrap/Container';
+
 import SignUp from './user/sign-up';
 import SignIn from './user/sign-in';
 import Home from './home';
 
 const Routes = () => {
 	return (
-		<Switch>
-			<Route path='/' exact component={Home} />
-			<Route path='/signin' exact component={SignIn} />
-			<Route path='/signup' exact component={SignUp} />
-		</Switch>
+		<Container className='mt-5'>
+			<Switch>
+				<Route path='/' exact component={Home} />
+				<Route path='/signin' exact component={SignIn} />
+				<Route path='/signup' exact component={SignUp} />
+			</Switch>
+		</Container>
 	);
 };
 
--- frontend/src/app/components/main/user/sign-up/SignUp.js
+++ frontend/src/app/components/main/user/sign-up/SignUp.js
@@ -1,7 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const SignUp = () => {
-	return <div>SignUp</div>;
+	const [formfields, setFormFields] = useState({
+		name: '',
+		email: '',
+		password: '',
+	});
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		console.log(formfields);
+	};
+
+	const onChange = (field) => (e) => {
+		setFormFields({
+			...formfields,
+			[field]: e.target.value,
+		});
+	};
+
+	return (
+		<Row>
+			<Col md={{ span: 8, offset: 2 }}>
+				<Form onSubmit={onSubmit}>
+					<Form.Group controlId='formName'>
+						<Form.Label>Name</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder='John'
+							onChange={onChange('name')}
+							value={formfields.name}
+							required
+							pattern='.{1,32}'
+							title='Name cannot extend 32 symbols'
+						/>
+					</Form.Group>
+
+					<Form.Group controlId='formEmail'>
+						<Form.Label>Email</Form.Label>
+						<Form.Control
+							type='email'
+							placeholder='email@mail.com'
+							onChange={onChange('email')}
+							value={formfields.email}
+							required
+						/>
+					</Form.Group>
+
+					<Form.Group controlId='formPassword'>
+						<Form.Label>Password</Form.Label>
+						<Form.Control
+							type='password'
+							onChange={onChange('password')}
+							value={formfields.password}
+							required
+							pattern='.{6,}'
+							title='Password must be at least 6 characters long'
+						/>
+						<Form.Text className='text-muted'>
+							Must be at least 6 characters long.
+						</Form.Text>
+					</Form.Group>
+
+					<Button variant='primary' type='submit'>
+						Submit
+					</Button>
+				</Form>
+			</Col>
+		</Row>
+	);
 };
 
 export default SignUp;
